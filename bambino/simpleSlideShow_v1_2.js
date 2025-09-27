bebeaa78d0d6f2646bbeb3c68a98a30703e43395
Meshach <meshach.sandesh@gether.com>
2025-09-27 16:42:50 +0530

    removed the caller from inside the simple slide show

--- bambino/simpleSlideShow_v1_2.js
+++ bambino/simpleSlideShow_v1_2.js
@@ -289,26 +289,3 @@ function createSlideShow(options = {}) {
     resumeAutoScroll,
   };
 }
-
-createSlideShow({
-  wrapper: "#slideWrapperLeg",
-  leftArrow: "#arrow-left",
-  rightArrow: "#arrow-right",
-  slideSelector: ".slideitem",
-  duration: 500,
-  easing: "cubic-bezier(.2,.9,.2,1)",
-  stepPercent: 100,
-  loop: true,
-  autoScroll: true,
-  autoScrollDelay: 7000,
-});
-createSlideShow({
-  wrapper: "#leg2slidewrapper",
-  leftArrow: "#arrow-left-l2",
-  rightArrow: "#arrow-right-l2",
-  slideSelector: ".leg2slideitem",
-  duration: 500,
-  easing: "cubic-bezier(.2,.9,.2,1)",
-  stepPercent: 100,
-  loop: false,
-});
